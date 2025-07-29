367c2df9b9f4ce371949415abe4a7d2a8e37c502
vdiana16 <valentina.pasca@stud.ubbcluj.ro>
2025-07-29 13:17:54 +0300

    Create Data Model

--- src/main/java/guru/springframework/SpringRestClientExamplesApplication.java
+++ src/main/java/guru/springframework/SpringRestClientExamplesApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.springrestclientexamples;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
